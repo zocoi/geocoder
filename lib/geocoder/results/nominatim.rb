fdd96e4999b557af6e71b302950049770e0fbf92
Hung Dao <mephis1987@gmail.com>
2017-01-31 00:43:52 -0800

    added state code

--- lib/geocoder/results/nominatim.rb
+++ lib/geocoder/results/nominatim.rb
@@ -3,6 +3,69 @@ require 'geocoder/results/base'
 module Geocoder::Result
   class Nominatim < Base
 
+    US_STATES = {
+      'alabama' => 'AL',
+      'alaska' =>	'AK',
+      'american samoa' =>	'AS',
+      'arizona' =>	'AZ',
+      'arkansas' =>	'AR',
+      'california' =>	'CA',
+      'colorado' =>	'CO',
+      'connecticut' =>	'CT',
+      'delaware' =>	'DE',
+      'district of columbia' => 'DC',
+      'florida' =>	'FL',
+      'georgia' =>	'GA',
+      'guam' =>	'GU',
+      'hawaii' =>	'HI',
+      'idaho' =>	'ID',
+      'illinois' =>	'IL',
+      'indiana' =>	'IN',
+      'iowa' =>	'IA',
+      'kansas' =>	'KS',
+      'kentucky' =>	'KY',
+      'louisiana' =>	'LA',
+      'maine' =>	'ME',
+      'maryland' =>	'MD',
+      'marshall islands' =>	'MH',
+      'massachusetts' =>	'MA',
+      'michigan' =>	'MI',
+      'micronesia' =>	'FM',
+      'minnesota' =>	'MN',
+      'mississippi' =>	'MS',
+      'missouri' =>	'MO',
+      'montana' =>	'MT',
+      'nebraska' =>	'NE',
+      'nevada' =>	'NV',
+      'new hampshire' => 'NH',
+      'new jersey' =>	'NJ',
+      'new mexico' =>	'NM',
+      'new york' =>	'NY',
+      'north carolina' =>	'NC',
+      'north dakota' => 'ND',
+      'northern marianas' => 'MP',
+      'ohio' =>	'OH',
+      'oklahoma' =>	'OK',
+      'oregon' =>	'OR',
+      'palau' =>	'PW',
+      'pennsylvania' =>	'PA',
+      'puerto rico' => 'PR',
+      'rhode island' =>	'RI',
+      'south carolina' =>	'SC',
+      'south dakota' =>	'SD',
+      'tennessee' =>	'TN',
+      'texas' =>	'TX',
+      'utah' =>	'UT',
+      'vermont' =>	'VT',
+      'virginia' =>	'VA',
+      'virgin islands' =>	'VI',
+      'washington' =>	'WA',
+      'west virginia' => 'WV',
+      'wisconsin' =>	'WI',
+      'wyoming' =>	'WY'
+    }
+
+
     def poi
       return @data['address'][place_type] if @data['address'].key?(place_type)
       return nil
@@ -46,7 +109,9 @@ module Geocoder::Result
       @data['address']['state']
     end
 
-    alias_method :state_code, :state
+    def state_code
+      US_STATES[state.downcase]
+    end
 
     def postal_code
       @data['address']['postcode']
